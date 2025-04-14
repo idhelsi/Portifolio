66d4cb5ba1ae1a24a075c689f8e8ce71170e7730
Helsi <helsi_@hotmail.com>
2025-04-14 15:46:38 -0400

    estilo para cada lingua

--- src/components/card-item.tsx
+++ src/components/card-item.tsx
@@ -1,6 +1,5 @@
 "use client"
  
-import { linguagemCores } from "@/data/linguagems"
 import { api } from "@/utils/api"
 import { useState, useEffect } from "react"
 
@@ -16,6 +15,20 @@ type Projeto = {
   }
 }
 
+export const linguagemCores: { [key: string]: string } = {
+  javascript: "bg-yellow-500",
+  typescript: "bg-blue-600",
+  php: "bg-purple-600",
+  html: "bg-red-500",
+  css: "bg-blue-500",
+  tailwind: "bg-cyan-500",
+  "c#": "bg-green-600",
+  react: "bg-blue-400",
+  next: "bg-black",
+  node: "bg-green-500",
+  laravel: "bg-red-600",
+  python: "bg-blue-800",
+};
 
 
 
